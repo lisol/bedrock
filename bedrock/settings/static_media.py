79320dc435aee1ff31b70a7a49b1532c0c39e96e
alexgibson <agibson@mozilla.com>
2016-11-01 10:15:27 +0000

    [fix bug 1313708] Yahoo Search Test /firstrun

--- bedrock/settings/static_media.py
+++ bedrock/settings/static_media.py
@@ -379,6 +379,12 @@ PIPELINE_CSS = {
         ),
         'output_filename': 'css/firefox_firstrun-horizon-bundle.css',
     },
+    'firefox_firstrun_yahoo_retention': {
+        'source_filenames': (
+            'css/firefox/firstrun/yahoo-retention.less',
+        ),
+        'output_filename': 'css/firefox_firstrun_yahoo_retention-bundle.css',
+    },
     'firefox_firstrun_ravioli': {
         'source_filenames': (
             'css/firefox/firstrun/ravioli.less',
@@ -1408,6 +1414,13 @@ PIPELINE_JS = {
             'js/firefox/whatsnew/whatsnew-zh-tw-49-init.js',
         ),
         'output_filename': 'js/firefox_whatsnew_zh_tw_49-bundle.js',
+    },
+    'firefox_firstrun_yahoo_retention': {
+        'source_filenames': (
+            'js/base/uitour-lib.js',
+            'js/firefox/firstrun/yahoo-retention.js',
+        ),
+        'output_filename': 'js/firefox_firstrun_yahoo_retention-bundle.js',
     },
     'geolocation': {
         'source_filenames': (
